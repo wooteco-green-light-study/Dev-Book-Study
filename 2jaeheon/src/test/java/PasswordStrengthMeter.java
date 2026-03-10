d0728a3495c6ef7197e0c9695d7499e8528cd4e9
2Jaeheon <jaeheon0826@jbnu.ac.kr>
2026-03-10 20:28:46 +0900

    test: 숫자를 포함하지 않고 나머지 조건은 충족하는 경우
    Signed-off-by: 2Jaeheon <jaeheon0826@jbnu.ac.kr>

--- 2jaeheon/src/test/java/PasswordStrengthMeter.java
+++ 2jaeheon/src/test/java/PasswordStrengthMeter.java
@@ -4,6 +4,22 @@ public class PasswordStrengthMeter {
             return PasswordStrength.NORMAL;
         }
 
+        boolean containsNum = meetsContainingNumberCriteria(s);
+        if (!containsNum) {
+            return PasswordStrength.NORMAL;
+        }
+
         return PasswordStrength.STRONG;
+    }
+
+    private static boolean meetsContainingNumberCriteria(String s) {
+        boolean containsNum = false;
+        for (char ch : s.toCharArray()) {
+            if (ch >= '0' && ch < '9') {
+                containsNum = true;
+                break;
+            }
+        }
+        return containsNum;
     }
 }
