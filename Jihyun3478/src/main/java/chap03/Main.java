84e864cdcd0af4c6d0c900b3183d8e58c58781af
Jihyun3478 <leejihyun3478@gmail.com>
2026-03-16 21:42:35 +0900

    feat: chapter 03까지 진행한 실습 코드 업데이트

---
+++ Jihyun3478/src/main/java/chap03/Main.java
@@ -0,0 +1,6 @@
+package chap03;
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Jihyun3478/src/main/java/chap03/PasswordStrength.java
@@ -0,0 +1,8 @@
+package chap03;
+
+public enum PasswordStrength {
+    STRONG,
+    NORMAL,
+    WEAK,
+    INVALID;
+}
