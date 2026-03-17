9176bfe0de8402bf676f934438345de70117ac30
Soojin6943 <sjhan0711@gmail.com>
2026-03-17 12:49:24 +0900

    test: 예외 상황 테스트 추가 & 코드 정리

--- Soojin6943/src/test/java/chap03/ExpiryDateCalculator.java
+++ Soojin6943/src/test/java/chap03/ExpiryDateCalculator.java
@@ -1,18 +1,31 @@
 package chap03;
 
 import java.time.LocalDate;
+import java.time.YearMonth;
 
 public class ExpiryDateCalculator {
 
     public LocalDate calculateExpiryDate(PayData payData) {
         int addedMonths = payData.getPayAmount() / 10_000;
         if(payData.getFirstBillingDate() != null) {
-            LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
-            if (payData.getFirstBillingDate().getDayOfMonth() != candidateExp.getDayOfMonth()) {
-                return candidateExp.withDayOfMonth(
-                        payData.getFirstBillingDate().getDayOfMonth());
-            }
+            return expiryDateUsingFirstBillingDate(payData, addedMonths);
+        } else {
+            return payData.getBillingDate().plusMonths(addedMonths);
         }
-        return payData.getBillingDate().plusMonths(addedMonths);
+    }
+
+    private LocalDate expiryDateUsingFirstBillingDate(
+            PayData payData, int addedMonths) {
+        LocalDate candidateExp = payData.getBillingDate().plusMonths(addedMonths);
+        final int dayOfFirstBilling = payData.getFirstBillingDate().getDayOfMonth();
+        if (dayOfFirstBilling != candidateExp.getDayOfMonth()) {
+            final int dayLenOfCandiMon = YearMonth.from(candidateExp).lengthOfMonth();
+            if (dayLenOfCandiMon < payData.getFirstBillingDate().getDayOfMonth()) {
+                return candidateExp.withDayOfMonth(dayLenOfCandiMon);
+            }
+            return candidateExp.withDayOfMonth(dayOfFirstBilling);
+        } else {
+            return candidateExp;
+        }
     }
 }
