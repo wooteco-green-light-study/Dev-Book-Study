f8caf4785f161ba2b906b8d2ed771de64612fcf3
Soojin6943 <sjhan0711@gmail.com>
2026-03-17 12:06:10 +0900

    refactor: 다음 테스트를 추가하기 전에 리팩토링

--- Soojin6943/src/test/java/chap03/ExpiryDateCalculator.java
+++ Soojin6943/src/test/java/chap03/ExpiryDateCalculator.java
@@ -3,7 +3,8 @@ package chap03;
 import java.time.LocalDate;
 
 public class ExpiryDateCalculator {
-    public LocalDate calculateExpiryDate(LocalDate billingDate, int payAmount) {
-        return billingDate.plusMonths(1);
+
+    public LocalDate calculateExpiryDate(PayData payData) {
+        return payData.getBillingDate().plusMonths(1);
     }
 }
