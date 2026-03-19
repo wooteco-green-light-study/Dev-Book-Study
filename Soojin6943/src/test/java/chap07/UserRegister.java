d9144e29188d40f194f0580cee2c3b7b862b8426
Soojin6943 <sjhan0711@gmail.com>
2026-03-19 16:59:19 +0900

    test: 이메일 발송 여부를 확인하기 위해 스파이를 사용

--- Soojin6943/src/test/java/chap07/UserRegister.java
+++ Soojin6943/src/test/java/chap07/UserRegister.java
@@ -3,10 +3,12 @@ package chap07;
 public class UserRegister {
     private WeakPasswordChecker passwordChecker;
     private UserRepository userRepository;
+    private EmailNotifier emailNotifier;
 
-    public UserRegister(WeakPasswordChecker passwordChecker, UserRepository userRepository) {
+    public UserRegister(WeakPasswordChecker passwordChecker, UserRepository userRepository, EmailNotifier emailNotifier) {
         this.passwordChecker = passwordChecker;
         this.userRepository = userRepository;
+        this.emailNotifier = emailNotifier;
     }
 
     public void register(String id, String pw, String email) {
@@ -18,5 +20,7 @@ public class UserRegister {
             throw new DupIdException();
         }
         userRepository.save(new User(id, pw, email));
+
+        emailNotifier.sendRegisterEmail(email);
     }
 }
--- Soojin6943/src/test/java/chap07/UserRegisterTest.java
+++ Soojin6943/src/test/java/chap07/UserRegisterTest.java
@@ -2,6 +2,7 @@ package chap07;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -11,10 +12,11 @@ public class UserRegisterTest {
     private UserRegister userRegister;
     private StubWeakPasswordChecker stubPasswordChecker = new StubWeakPasswordChecker();
     private MemoryUserRepository fakeRepository = new MemoryUserRepository();
+    private SpyEmailNotifier spyEmailNotifier = new SpyEmailNotifier();
 
     @BeforeEach
     public void setUp() {
-        userRegister = new UserRegister(stubPasswordChecker, fakeRepository);
+        userRegister = new UserRegister(stubPasswordChecker, fakeRepository, spyEmailNotifier);
     }
 
     @DisplayName("약한 암호면 가입 실패")
@@ -46,5 +48,17 @@ public class UserRegisterTest {
         User savedUser = fakeRepository.findById("id");
         assertEquals("id", savedUser.getId());
         assertEquals("email", savedUser.getEmail());
+    }
+
+    @DisplayName("가입하면 메일을 전송함")
+    @Test
+    void whenRegisterThenSendMail() {
+        userRegister.register("id", "pw", "email@email.com");
+
+        assertTrue(spyEmailNotifier.isCalled());
+        assertEquals(
+                "email@email.com",
+                spyEmailNotifier.getEmail()
+        );
     }
 }
